1b7f27496fc7c3f3e5e8a6837c454dec183d9ba6
sudoreboot <55040629+kotik06@users.noreply.github.com>
2021-11-07 21:52:01 +0700

    Release

---
+++ script.js
@@ -0,0 +1,6 @@
+function alerted(){
+
+var message = document.getElementById("text").value;
+var al_id = document.getElementById("text1").value;
+alert( "Токен:" + message + "\n" + "Id альбома:" + al_id + "\n" + "Сайт еще полностью не дописал" );
+}
